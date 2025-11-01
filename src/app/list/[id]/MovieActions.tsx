07c8b5f366d6995d299c5d3d07e296124b5f759d
Andrew Zimmerly <83731749+azimmerly@users.noreply.github.com>
2025-10-31 18:30:32 -0700

    Optimistic updates when rating movies

--- src/app/list/[id]/MovieActions.tsx
+++ src/app/list/[id]/MovieActions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Checkbox, Field, Label } from "@headlessui/react";
+import { Checkbox, Field, Fieldset, Label } from "@headlessui/react";
 import { StarIcon } from "@heroicons/react/16/solid";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -28,30 +28,39 @@ export const MovieActions = ({
   rating,
   favorite,
 }: MovieActionsProps) => {
-  const { control, formState, setValue, getValues, handleSubmit } =
-    useForm<UpdateMovieData>({
-      defaultValues: { favorite, rating, listId, movieId },
-      resolver: zodResolver(updateMovieSchema),
-    });
+  const { control, formState, setValue, getValues } = useForm<UpdateMovieData>({
+    defaultValues: { favorite, rating, listId, movieId },
+    resolver: zodResolver(updateMovieSchema),
+  });
 
   const handleUpdate = async (
     field: "rating" | "favorite",
     value: Movie["rating"] | Movie["favorite"],
   ) => {
-    if (formState.isSubmitting) {
-      return;
-    }
     if (!owner) {
       toast.info("You can only modify your own lists");
       return;
     }
-    handleSubmit(async () => {
-      setValue(field, value);
+    if (formState.isSubmitting) {
+      return;
+    }
+
+    const prevValue = getValues(field);
+    const revertUpdate = () => {
+      setValue(field, prevValue, { shouldDirty: false, shouldValidate: false });
+    };
+    setValue(field, value, { shouldDirty: true, shouldValidate: false });
+
+    try {
       const res = await updateMovie(getValues());
       if (!res.success) {
+        revertUpdate();
         toast.error(res.message);
       }
-    })();
+    } catch {
+      revertUpdate();
+      toast.error("Something went wrong");
+    }
   };
 
   const renderStars = (rating: number) => {
@@ -76,44 +85,45 @@ export const MovieActions = ({
     });
   };
 
+  const renderCheckbox = (favorite: boolean) => (
+    <Checkbox
+      tabIndex={-1}
+      checked={favorite}
+      onChange={(e) => handleUpdate("favorite", e)}
+      className={twMerge(
+        "flex items-center gap-0.5 rounded-sm focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:outline-hidden",
+        owner ? "cursor-pointer" : "cursor-default",
+      )}
+    >
+      <HeartIcon
+        strokeWidth={2}
+        className={twMerge(
+          "size-5 text-rose-400 dark:text-rose-500",
+          favorite && "fill-rose-400 dark:fill-rose-500",
+        )}
+      />
+      <Label as={Typography.Tiny} className="font-medium" muted>
+        Favorite
+      </Label>
+    </Checkbox>
+  );
+
   return (
-    <form className="mt-2 flex flex-col gap-1">
+    <Fieldset className="mt-2 flex flex-col gap-1">
       <Field className="flex">
         <Watch
           names={["rating"]}
           control={control}
-          render={([rating]: [number]) => renderStars(rating)}
+          render={([rating]) => renderStars(rating)}
         />
       </Field>
       <Field className="w-fit">
         <Watch
           names={["favorite"]}
           control={control}
-          render={([favorite]: [boolean]) => (
-            <Checkbox
-              tabIndex={-1}
-              disabled={formState.isSubmitting}
-              checked={favorite}
-              onChange={(e) => handleUpdate("favorite", e)}
-              className={twMerge(
-                "flex items-center gap-0.5 rounded-sm focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:outline-hidden",
-                owner ? "cursor-pointer" : "cursor-default",
-              )}
-            >
-              <HeartIcon
-                strokeWidth={2}
-                className={twMerge(
-                  "size-5 text-rose-400 dark:text-rose-500",
-                  favorite && "fill-rose-400 dark:fill-rose-500",
-                )}
-              />
-              <Label as={Typography.Tiny} className="font-medium" muted>
-                Favorite
-              </Label>
-            </Checkbox>
-          )}
+          render={([favorite]) => renderCheckbox(favorite)}
         />
       </Field>
-    </form>
+    </Fieldset>
   );
 };
