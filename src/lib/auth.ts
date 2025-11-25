5a5e44c05657a58a220b1da5337bc10725393f8b
Andrew Zimmerly <83731749+azimmerly@users.noreply.github.com>
2025-11-25 06:22:37 -0800

    Update betterAuth import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,5 @@
-import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { betterAuth } from "better-auth/minimal";
 
 import { cleanupUnreferencedMovieInfo } from "@/actions/utils";
 import { APP_NAME } from "@/consts";
