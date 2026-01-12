00307fe898cae58e6d8caf499cf5dfa722df933d
Andrew Zimmerly <83731749+azimmerly@users.noreply.github.com>
2026-01-12 08:45:20 -0800

    Update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,18 +3,15 @@ import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
 
 const NotFound = () => (
-  <div className="text-center">
-    <Typography.Body className="font-semibold text-blue-600 dark:text-blue-500">
-      404
-    </Typography.Body>
-    <Typography.H1 className="mt-2">Page not found</Typography.H1>
-    <Typography.Body className="mt-6" muted>
+  <div className="mt-20 text-center">
+    <Typography.H1>Page not found</Typography.H1>
+    <Typography.Body className="mt-4" muted>
       Sorry, we couldn’t find the page you’re looking for.
     </Typography.Body>
     <Link
       replace
       href="/"
-      className="mx-auto mt-10 flex w-fit items-center gap-1 rounded-sm p-1 text-sm font-semibold hover:opacity-70 focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:outline-hidden"
+      className="mx-auto mt-8 flex w-fit items-center gap-1 rounded-sm p-1 text-sm font-semibold hover:opacity-70 focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:outline-hidden"
     >
       Go back
       <ArrowRightIcon className="size-4" />
